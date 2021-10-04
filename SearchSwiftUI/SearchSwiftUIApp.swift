dd5d4d0d39278ce9fc835ade884b493b62a61089
Mohamed JABERI <mohamed.jaberi@externe.e-i.com>
2021-10-04 16:22:46 +0200

    Initial Commit

---
+++ SearchSwiftUI/SearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchSwiftUIApp.swift
+//  SearchSwiftUI
+//
+//  Created by eidd5180 on 04/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
